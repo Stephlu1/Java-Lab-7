7c44681ab9312c49beba2429f5527f8343166005
Luanna <n01704752@humber.ca>
2025-04-04 21:41:41 -0400

    added Script for form event

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //Lab 7: JavaScript Events
 
 //Mouse Events
-const mouseBtn = mdocument.getElementById('mouseButton')
-const mouseBtn = mdocument.getElementById('mouseMessage')
+var mouseBtn = mdocument.getElementById('mouseButton')
+var mouseBtn = mdocument.getElementById('mouseMessage')
 
-mouseBtn.addEventListener('mouseover', function() {
+mouseBtn.addEventListener('mouseenter', function() {
     mouseMsg.textContent ="You're blocking the light";
 });
 mouseBtn.addEventListener('mouseleave', function() {
     mouseMsg.textContent ="Wait come back!";
 });
+
+//Keyboard Events
+document.addEventListener('keypress', function(event) {
+    console.log(`Character: ${event.charCode}`); 
+    console.log(`Character pressed: ${String.fromCharCode(event.charCode)}`);
+});
+
+//Form Event
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('demoForm');
+    const formMsg = document.getElementById('formMessage');
+
+    form.addEventListener('submit', function(event) {
+        console.log('Form submitted');
+        event.preventDefault(); 
+        formMsg.textContent = "Form successfuly submited";
+    });
